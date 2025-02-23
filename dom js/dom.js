3642db7c4dca3103a5a45fb54e2b8437e36f265f
abufatttahmdshakib <abufattahmdshakib21@gmail.com>
2025-02-24 02:22:47 +0600

    onclike related

--- dom js/dom.js
+++ dom js/dom.js
@@ -10,11 +10,18 @@ makeBlue.onclick = function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
 
- document.getElementById('golden').addEventListener('click', function(){
-document.body.style.backgroundColor = 'goldenrod';
+document.getElementById('golden').addEventListener('click', function () {
+    document.body.style.backgroundColor = 'goldenrod';
 
- })
+})
 
- document.getElementById('btn-make-color').addEventListener('click', function() {
-    document.body.style.background = 'red'; 
+document.getElementById('btn-make-color').addEventListener('click', function () {
+    document.body.style.background = 'red';
+})
+
+document.getElementById('btn-update').addEventListener('click', function () {
+    const nameInput = document.getElementById('input-name');
+    const name = nameInput.value;
+    const finalupdate = document.getElementById('update-text');
+    finalupdate.innerText = name;
 })
